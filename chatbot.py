784fd39084c55758ceb63461c010d7202400e2a7
Tusharshah3 <tusharshah372003@gmail.com>
2025-04-20 22:36:20 +0530

    update port

--- chatbot.py
+++ chatbot.py
@@ -207,4 +207,4 @@ def health_check():
 
 if __name__ == '__main__':
     # Make sure your environment variables are set, then:
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", 3000))
